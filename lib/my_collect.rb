f50cc5e1f3ba9c0d39950a9ec921476ce660946f
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-07 22:17:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+# take an argument of a collection,
+# run through the collection 
+# execute the code for each collection 
+# return the collection, modified
 
 
+def my_collect(array)
+  
+  i = 0 
+  collect = []
+  
+  while i < array.length 
+    collect << yield(array[i])
+    i += 1
+  end
+  
+  collect 
+end
+
+# collection = ['ruby', 'javascript', 'python', 'objective-c']
+# students = ["Tim Jones", "Tom Smith", "Jim Campagno", "Marcie Martian", "Vane Rodriguez", "Laya Murphy", "Gabriel Casey"]
+
+# puts my_collect(students) {|student| "Welcome MR. #{student.split(" ").last.upcase} to class."}
